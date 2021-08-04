39e2963b3a8358f9a419cea7f6d1ebe1ffe8a770
k1222 <243911961@qq.com>
2021-08-04 17:29:32 +0800

    添加TimePicker逻辑
    添加TimePicker逻辑

--- app/src/main/java/com/example/projectdemo/MainActivity.java
+++ app/src/main/java/com/example/projectdemo/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.projectdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.text.DateFormat;
 import java.util.Calendar;
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         Calendar calendar = Calendar.getInstance();
         DatePicker datePicker = findViewById(R.id.data_picker);
         TextView date = findViewById(R.id.tv_date);
+        TimePicker timePicker = findViewById(R.id.time_picker);
+        TextView time = findViewById(R.id.tv_time);
         datePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_YEAR), new DatePicker.OnDateChangedListener() {
             @Override
             public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
@@ -28,6 +32,22 @@ public class MainActivity extends AppCompatActivity {
                 instance.set(Calendar.DAY_OF_MONTH,dayOfMonth);
                 String format = DateFormat.getDateInstance(DateFormat.MEDIUM).format(new Date(instance.getTimeInMillis()));
                 date.setText(format);
+            }
+        });
+        //是否显示24小时视图
+        timePicker.setIs24HourView(false);
+        //设置选中的时
+        timePicker.setCurrentHour(2);
+        //设置选中的分
+        timePicker.setCurrentMinute(59);
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                Log.e("onTimeChanged",hourOfDay+" "+minute);
+                Calendar timeCalendar = Calendar.getInstance();
+                timeCalendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                timeCalendar.set(Calendar.MINUTE,minute);
+                time.setText(android.text.format.DateFormat.getTimeFormat(getBaseContext()).format(new Date(timeCalendar.getTimeInMillis())));
             }
         });
     }
