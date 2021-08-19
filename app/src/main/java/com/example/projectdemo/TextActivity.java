d7864921287a23698c6eb3da3d47318066212fd6
k1222 <243911961@qq.com>
2021-08-19 22:07:55 +0800

    添加TextView自动缩小字体
    添加TextView自动缩小字体

--- app/src/main/java/com/example/projectdemo/TextActivity.java
+++ app/src/main/java/com/example/projectdemo/TextActivity.java
@@ -1,9 +1,11 @@
 package com.example.projectdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.TextViewCompat;
 
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
 
@@ -16,6 +18,9 @@ public class TextActivity extends AppCompatActivity {
         TextView delete = findViewById(R.id.tv_delete);
         delete.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG);
         TextView underLine = findViewById(R.id.tv_under_line);
+        TextView auto = findViewById(R.id.tv_auto);
         underLine.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+        TextViewCompat.setAutoSizeTextTypeWithDefaults(auto,TextView.AUTO_SIZE_TEXT_TYPE_UNIFORM);
+        TextViewCompat.setAutoSizeTextTypeUniformWithPresetSizes(auto,getResources().getIntArray(R.array.autosize_text_sizes), TypedValue.COMPLEX_UNIT_SP);
     }
 }
