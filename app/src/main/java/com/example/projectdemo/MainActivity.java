817163b455ec025a9a45cab020c020874f0b928c
k1222 <243911961@qq.com>
2021-08-27 16:54:52 +0800

    添加LinearLayoutActivity页跳转
    添加LinearLayoutActivity页跳转

--- app/src/main/java/com/example/projectdemo/MainActivity.java
+++ app/src/main/java/com/example/projectdemo/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn_edit).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, EditTextActivity.class)));
         findViewById(R.id.btn_text_input).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, TextInputLayoutActivity.class)));
         findViewById(R.id.btn_floating_action_button).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, FloatingActionButtonActivity.class)));
+        findViewById(R.id.btn_linear_layout).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, LinearLayoutActivity.class)));
 //        viewModel = new ViewModelProvider(this).get(TestViewModel.class);
 //        TestRepo.getInstance().getDataLive("")
 //                .observe(this, new Observer<List<Object>>() {
